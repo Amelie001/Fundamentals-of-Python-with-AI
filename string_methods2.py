a4ca566a41d6318c8d83b883f2e475a37db3c3b1
Amelie001 <amelie.iaconis@gmail.com>
2026-02-21 19:24:55 +0000

    Add files via upload

---
+++ string_methods2.py
@@ -0,0 +1,22 @@
+text = input("Enter a string to test:")
+
+print("RESULTS")
+
+print("find('a'):", text.find("a"))
+print("count('a'):", text.count("a"))
+print("startswith('A'):", text.startswith("A"))
+print("endswith('.'):", text.endswith("."))
+
+print("isalnum():", text.isalnum())
+print("isalpha():", text.isalpha())
+print("isdigit():", text.isdigit())
+print("isnumeric():", text.isnumeric())
+
+print("islower():", text.islower())
+print("isupper():", text.isupper())
+print("istitle():", text.istitle())
+
+print("isspace():", text.isspace())
+print("isprintable():", text.isprintable())
+print("isascii():", text.isascii())
+print("isidentifier():", text.isidentifier())
