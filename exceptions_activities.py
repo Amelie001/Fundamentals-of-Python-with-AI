1cc441e422464bcc9cae4209aef3ba89047158f8
Amelie001 <amelie.iaconis@gmail.com>
2026-03-17 11:03:04 +0000

    Add files via upload

--- exceptions_activities.py
+++ exceptions_activities.py
@@ -1,5 +1,5 @@
 # Activity 1
-'''
+
 try: 
     people = int(input("How many people? "))
     total_cost = float(input("What is your total cost? "))
@@ -10,7 +10,7 @@ else:
     print("Your ticket cost per person is: £", ticket_cost)
 finally:
     print("Come back soon!")
-'''
+
 
 # Activity 2
 
@@ -26,3 +26,104 @@ try:
         print("Withdrawl successful. Remaining balance is:", balance)
 except ValueError as e:
     print("Error:", e)
+
+# Activity 3 
+
+try: 
+    password = input("Enter your password: ")
+
+    if len(password) < 6: 
+        raise ValueError("Password is too short (minimum 6 characters).")
+    
+except ValueError as e:
+    print("Error:", e)
+
+else: 
+    print("Password accepted.")
+
+# Activity 4
+
+try: 
+    student_name = input("Enter student name: ")
+    marks = float(input("Enter student marks: "))
+
+    if marks < 0 or marks > 100:
+        raise ValueError("Marks cannot be less than 0 or greater than 100.")
+    
+    print(student_name, "\n", marks)
+    
+    if marks >= 90:
+        print("Grade A")
+    elif marks >= 75:
+        print("Grade B")
+    elif marks >= 50: 
+        print("Grade C")
+    else: 
+        print("Grade Fail")
+    
+except ValueError as e:
+    print("Error:", e)
+
+# Activity 5 
+
+try: 
+    filename = input("Enter file name: ")
+
+    if filename == "": 
+        raise ValueError("File name cannot be empty.")
+    
+    file = open(filename, "r")
+    content = file.read()
+    print("File content:\n", content)
+    
+    file.close()
+
+except FileNotFoundError: 
+    print("File not found.")
+
+except ValueError as e:
+    print("Error:", e)
+
+# Activity 6
+
+try: 
+    movie = input("Enter movie name: ")
+    tickets = int(input("Enter number of tickets: "))
+
+    if tickets <= 0:
+        raise ValueError("Number of tickets cannot be less than 1.")
+    
+    price = 250 
+    total_cost = tickets * price 
+
+    print("Booking details:")
+    print("Movie:", movie)
+    print("Ticket price:", price)
+    print("Tickets booked:", tickets)
+    print("Total cost:", total_cost)
+
+except ValueError as e:
+    print("Error:", e)
+
+# Activity 7 
+
+try: 
+    filename = input("Enter file name: ")
+
+    if filename == "":
+        raise ValueError("File name cannot be empty.")
+    
+    file = open(filename, "r")
+    file.close()
+
+    file = open(filename, "a")
+    content = input("What would you like to add? ")
+    file.write(content)
+    print("Content added successfully.")
+    file.close()
+
+except ValueError as e:
+    print("Error:", e)
+
+except FileNotFoundError:
+    print("File not found.")
