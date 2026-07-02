96136224fd1223d84cfeda99d27517b14520e6e2
Amelie001 <amelie.iaconis@gmail.com>
2026-07-02 10:05:38 +0100

    Add files via upload

---
+++ sets_homework.py
@@ -0,0 +1,12 @@
+A = {2, 4, 6, 8, 10}
+B = {4, 5, 6, 7, 8}
+
+print("Union:", A | B)
+
+print("Intersection:", A & B)
+
+print("Difference (A - B):", A - B)
+
+print("Difference (B - A):", B - A)
+
+print("Symmetric Difference:", A ^ B)
